b6e80ad4e40242ab68722e1024914e2400d10965
신성현 <froggy0m0a@gmail.com>
2024-04-23 11:06:43 +0900

    init project

---
+++ springback/src/main/java/com/example/springback/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.springback.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@Slf4j
+public class TestController {
+    private final LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    private static int count = 0;
+
+    @Value("${test.var}")
+    private String var;
+
+    @GetMapping
+    public String getMapping() {
+        log.info("호출횟수={} localDateTime={}", count++, localDateTime.toString());
+        return "호출횟수 " + count + " var = " + var +  " -- " + localDateTime.toString() ;
+    }
+}
